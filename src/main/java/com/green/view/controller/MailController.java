37f8d36b5fee61ae9648ee5b8f80cede343d3d70
jaehyeok <88532077+22hyeok@users.noreply.github.com>
2021-09-29 00:06:16 +0900

    Update MailController.java
    로그인한 USER EmailID를 받기위한 부분 추가

--- src/main/java/com/green/view/controller/MailController.java
+++ src/main/java/com/green/view/controller/MailController.java
@@ -1,9 +1,11 @@
 package com.green.view.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.green.biz.dto.MailVO;
 import com.green.biz.mail.MailService;
@@ -21,8 +23,8 @@ public class MailController {
 	}
 	
 	@RequestMapping(value="send_email", method = RequestMethod.POST)
-	public String MailWrite(MailVO vo) {			
-		mailService.insertMail(vo);			
-		return "customer_center/customer_center";		//諛��≫��怨� customer_center.jsp
+	public String MailWrite(MailVO vo, @RequestParam(value="emailID") String email_id) {
+			mailService.insertMail(email_id, vo);			
+			return "customer_center/customer_center";		//발송 완료 후 customer_center.jsp
 	}
 }
